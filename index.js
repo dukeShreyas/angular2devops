0b09304976aee118b0b4e0f4d081335d8d2138cb
dukeShreyas <duke.shreyas@gmail.com>
2017-09-06 15:49:07 +0530

    test

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var childProcess = require('child_process')
 
 childProcess.exec('npm test', (err, stdout, stderr) => {
-	console.log(stdout)
+	if(stdout.indexOf('test failed') === -1 || stdout.indexOf('tests failed') === -1){
+		childProcess.exec('npm run build', (err, stdout, stderr) => {
+			console.log(stdout)
+		})
+	}
+	else {
+		console.log('Aborting build as at least 1 test case(s) are failed')
+	}
 })
