26c151531eb7e03753402e1b46d1651713764adf
dukeShreyas <duke.shreyas@gmail.com>
2017-09-08 10:37:02 +0530

    karma config

--- karma.conf.js
+++ karma.conf.js
@@ -6,3 +6,8 @@
  * Look in ./config for karma.conf.js
  */
 module.exports = require('./config/karma.conf.js');
+singleRun = true;
+reporters = ['dots', 'junit'];
+junitReporter = {
+  outputFile: 'test-results.xml'
+};
